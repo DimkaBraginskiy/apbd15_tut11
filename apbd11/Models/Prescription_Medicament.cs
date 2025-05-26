2bceea174a54aa63e72d1860e08c8d06e4e84e31
Dmytro Brahinskyi <dbraginskiy821@gmail.com>
2025-05-26 22:19:26 +0200

    feat(Models): add Models

---
+++ apbd11/Models/Prescription_Medicament.cs
@@ -0,0 +1,13 @@
+﻿namespace apbd11.Models;
+
+public class Prescription_Medicament
+{
+    public int IdMedicament { get; set; }
+    public Medicament Medicament { get; set; }
+    
+    public int IdPrescription { get; set; }
+    public Prescription Prescription { get; set; }
+    
+    public int Dose { get; set; }
+    public string Details { get; set; }
+}
